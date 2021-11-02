1da1146f519cbd15860c9cf0a10f228c6a3e2b60
Cornelius Smith <nealsmithg@gmail.com>
2021-11-02 13:55:48 -0400

    added delete to Tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -55,6 +55,16 @@ router.put('/:id', async(req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({where:{id: req.params.id}})
+    if (!tagData){
+      res.status(404).json({message: "No item by that tag."});
+      return;
+    }
+    res.status(200).json(tagData);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
