173bcbf563e26c9a5d9da8d06ace2ba8927e20a3
Cornelius Smith <nealsmithg@gmail.com>
2021-11-02 14:14:51 -0400

    added delete to category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -53,8 +53,18 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoriesData = await Tag.destroy({where:{id: req.params.id}})
+    if (!categoriesData){
+      res.status(404).json({message: "No category by that ID."});
+      return;
+    }
+    res.status(200).json(categoriesData);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
